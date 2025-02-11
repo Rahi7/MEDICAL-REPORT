998cd02dd646bf6ec1252ebb94530d3144316ee9
Saurabh Kumar <97509038+Rahi7@users.noreply.github.com>
2025-02-11 23:41:42 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import ProtectedRoute from './components/ProtectedRoute'
 import Home from './pages/Home'
-import Header from './components/Header'
-import Project from './pages/Project'
-import About from './pages/About';
-import Footer from './components/Footer'
-import Signin from './pages/SignIn'
-import Signup from './pages/SignUp'
-import Form from './pages/Form'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import DoctorRegister from './pages/DoctorRegister'
+import PatientRegister from './pages/PatientRegister'
+import About from './pages/About'
+import Services from './pages/Services'
+import Blog from './pages/Blog'
+import ForgotPassword from './pages/ForgotPassword'
 
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-    <Header></Header>
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path='/project' element={<Project />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/signin' element={<Signin />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/form' element={<Form />} />
-      </Routes>
-      <Footer></Footer>
-    </BrowserRouter>
-    
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public routes */}
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="doctor-register" element={<DoctorRegister />} />
+        <Route path="patient-register" element={<PatientRegister />} />
+        <Route path="about" element={<About />} />
+        <Route path="services" element={<Services />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+
+        {/* Protected routes */}
+        <Route element={<ProtectedRoute />}>
+          {/* Add protected routes here */}
+        </Route>
+      </Route>
+    </Routes>
   )
 }
 
--- main.jsx
+++ main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import App from './App'
 import './index.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <ToastContainer position="top-right" autoClose={3000} />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>,
 )
